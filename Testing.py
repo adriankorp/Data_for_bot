6a5fbd2265894d581fbe723230063c94b2cd25cc
Adrian <adis112211@gmail.com>
2020-11-12 13:54:44 +0100

    Working bot. Need add close signal!!

--- Testing.py
+++ Testing.py
@@ -9,14 +9,11 @@ from telethon.tl.types import InputUser, InputPeerUser, ReplyKeyboardMarkup, Rep
     PeerChannel
 
 
+
 conn = sqlite3.connect('Message_data.db')
 cur = conn.cursor()
 
-# client = TelegramClient(phone, api_id, api_hash)
-# client.connect()
-# if not client.is_user_authorized():
-#     client.send_code_request(phone)
-#     client.sign_in(phone, input('Enter the code: '))
+
 
 async def click_bottom(message: object, button_name = None, check = None):
     """
@@ -46,13 +43,14 @@ def read_from_data_base():
     cur.execute(f'SELECT * FROM "MESSAGES"')
     wiadomosci = cur.fetchall()
     result = [x[1] for x in wiadomosci]
-    return result
+    result_msg = [x[2] for x in wiadomosci]
+    return [result,result_msg]
 
 
 def save_data_to_base(data):
     sprawdzic = read_from_data_base()
     for i in data:
-        if i[1] not in sprawdzic:
+        if i[1] not in sprawdzic[0]:
             cur.execute(f'''INSERT INTO 'MESSAGES' VALUES (?,?,?)''', (None, i[1],i[0]))
             conn.commit()
 
@@ -87,6 +85,8 @@ async def get_message_from_channels() -> List:
                 1141692661,
                 1410247888,
                 1474777826,
+                1475063825,
+                1436257183,
                 ]
 
     messages = []
@@ -120,28 +120,30 @@ def change_to_BTCUSDT(mesagge):
 
 
 def check_message_is_signal(message):
-    KEY_WORDS = ['Entry', 'Zone', 'Take', 'Profit', 'Targets', 'Stop', 'ENTRY', 'STOP', 'Target']
-    message = message.split()
+    KEY_WORDS = ['Entry', 'Zone', 'Take', 'Profit', 'Targets', 'Stop', 'ENTRY', 'STOP', 'Target', 'AROUND', 'SEL', 'BUY', 'SHORT', 'FUTURES CALL', 'LONG']
+    message = message
     quantity = 0
-    for word in message:
-        if word in KEY_WORDS:
-            print('cos')
-            quantity += 1
+    if not 'Profit:' in message or not'Period' in message:
+        for word in KEY_WORDS:
+            if word in message:
+                quantity += 1
     if quantity >= 2:
         return True
     else:
         return False
 
+def check_message_is_close_signal(message):
+    KEY_WORDS = ['Close', 'CLOSE', 'Closed', 'CLOSED']
+
 async def send_signal(entity, message):
     from_bot = await get_message_from_bot(entity)
     if not 'Main Menu:' in from_bot[0].message:
         await click_bottom(message=from_bot, button_name='⇱ Main Menu')
-
+    time.sleep(2)
     if 'Main Menu' in from_bot[0].message:
         from_bot = await get_message_from_bot(entity)
         await click_bottom(message=from_bot, button_name='Signals')
-
-
+    time.sleep(2)
     from_bot = await get_message_from_bot(entity)
     if 'Choose an option below.' in from_bot[0].message:
         await click_bottom(message=from_bot, button_name='Import')
@@ -173,19 +175,38 @@ async def send_signal(entity, message):
 
 
 
-async def main():
-    signals = []
-    mesagges = await get_message_from_channels()
-    print(mesagges)
-    for msg in mesagges:
-        if check_message_is_signal(message=msg[0]):
-            change_to_BTCUSDT(msg)
-            signals.append(msg)
-    check = read_from_data_base()
-    for signal in signals:
-        if signal[1] not in check:
 
-    save_data_to_base(signals)
+async def main():
+    entity = await client.get_entity('cornix_trading_bot')
+    while True:
+        time.sleep(20)
+        signals = []
+        close = []
+        mesagges = await get_message_from_channels()
+
+        for msg in mesagges:
+            if check_message_is_signal(message=msg[0]):
+                change_to_BTCUSDT(msg)
+                signals.append(msg)
+        check = read_from_data_base()
+        print(signals)
+        for signal in signals:
+            if signal[1] not in check[0] and signal[0] not in check[1]:
+                await send_signal(entity,signal[0])
+                save_data_to_base([signal])
+
+
+
+
+#     mess = '''VİTE USDT 7X
+#
+# BUY 1700-1795
+#
+# SEL-2100-2300-
+#
+# 2X TO 3X'''
+#     print(check_message_is_signal(message=mess))
+
     # channel_entity = await client.get_entity('cornix_trading_bot')
     # await send_signal(channel_entity, message=message)
 
@@ -197,6 +218,7 @@ async def main():
 
 
 
+
     # for message in message_from_get_channel:
     #     print(message)
     # row, column = find_button_index(message_from_get_channel, 'Signals')
@@ -204,10 +226,10 @@ async def main():
 
 
 with TelegramClient(phone, api_id, api_hash) as client:
-    # if not  client.is_user_authorized():
-    #      client.send_code_request(phone)
-    #      client.sign_in(phone, input('Enter the code: '))
+    if not  client.is_user_authorized():
+         client.send_code_request(phone)
+         client.sign_in(phone, input('Enter the code: '))
 
     client.loop.run_until_complete(main())
 
-        # '''Trade was opened successfully'''
+
